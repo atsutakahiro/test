53c87ccaf0b0cb7842fdc1dfddabe6f2c1101c5f
atsutakahiro <takahiro619z@icloud.com>
2022-08-23 07:20:30 +0000

    c12

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  get 'tasks/:id', to: 'tasks#show'
   
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
          
-  resources :users
+  resources :users do
+    resources :tasks
+  end
 end
        
